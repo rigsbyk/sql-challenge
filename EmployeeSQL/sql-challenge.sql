b0c10a25c0ce2b1c4c645d353efb8b910aa55842
rigsbyk <kathryn.rigsby@gmail.com>
2020-08-13 14:50:57 -0400

    adding remainder files of table schemata, queries,and updated bonus

---
+++ EmployeeSQL/sql-challenge.sql
@@ -0,0 +1,73 @@
+--1.) List the following details of each employee: employee number, last name, 
+--first name, sex, and salary.
+
+Select e.emp_no,e.first_name,e.last_name,e.sex,s.salary
+from employees as e 
+inner join salaries as s on
+e.emp_no = s.emp_no;
+
+--2.) List first name, last name, and hire date for employees 
+--who were hired in 1986.
+
+Select first_name, last_name, hire_date
+from employees 
+where date_part('year',hire_date) = 1986;
+
+--3.)List the manager of each department with the following information: department number, 
+--department name, the manager's employee number, last name, first name.
+
+--* I added emp_title_id to make sure I was pulling all managers
+
+select d.dept_no, d.dept_name, e.emp_no, e.first_name,e.last_name,e.emp_title_id
+from departments as d
+inner join dept_manager as dm on
+d.dept_no = dm.dept_no 
+inner join employees as e on 
+dm.emp_no = e.emp_no;
+
+--4.)List the department of each employee with the following information: 
+--employee number, last name, first name, and department name.
+
+Select e.emp_no,e.first_name,e.last_name,d.dept_name
+From employees as e 
+Inner Join dept_emp as de
+On e.emp_no = de.emp_no 
+Inner Join departments as d 
+On de.dept_no = d.dept_no;
+
+--5.)List first name, last name, and sex for employees whose first name is
+--"Hercules" and last names begin with "B."
+Select first_name,last_name, sex from employees
+where first_name = 'Hercules' AND last_name ILIKE 'B%';
+
+--6.)List all employees in the Sales department, including their 
+--employee number, last name, first name, and department name.
+
+Select e.emp_no,e.first_name,e.last_name,d.dept_name
+From employees as e inner join 
+dept_emp as de On
+e.emp_no = de.emp_no
+inner join departments as d
+On de.dept_no = d.dept_no
+Where d.dept_name = 'Sales';
+
+--7.)List all employees in the Sales and Development departments, 
+--including their employee number, last name, first name, and department name.
+
+Select e.emp_no,e.first_name,e.last_name,d.dept_name
+From employees as e inner join 
+dept_emp as de On
+e.emp_no = de.emp_no
+inner join departments as d
+On de.dept_no = d.dept_no
+Where (d.dept_name = 'Sales') OR (d.dept_name='Development');
+
+--8.)In descending order, list the frequency count of employee last names, i.e.,
+--how many employees share each last name.
+
+Select last_name,count(last_name)as "Last Name Count"
+From employees
+Group by last_name
+Order by last_name desc;
+
+
